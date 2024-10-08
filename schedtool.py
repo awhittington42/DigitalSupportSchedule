25cecbfb93c0a23c34aca9d01771b7d0f7099405
awhittington42 <n01391420@unf.edu>
2024-10-08 16:31:50 -0400

    First Commit for Schedule Tool

---
+++ schedtool.py
@@ -0,0 +1,74 @@
+"""
+This script will be used for building out the Digital Support schedule.
+"""
+from calendar import Calendar
+import datetime
+import json
+
+MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+YEAR = 2025
+
+class tournament(object):
+    def __init__(self, name, id, startDate, endDate) -> None:
+        self.name = name
+        self.id = id
+        self.start_date = startDate
+        self.end_date = endDate
+        self.url = tournament.buildURL(name, id)
+        self.activeState = self.isActive()
+        print("active state = " + str(self.activeState))
+    
+    def isActive(self) -> bool:
+        curDay = datetime.date.today()
+        curWeek = curDay.isocalendar().week
+        print(curWeek)
+        print()
+
+        if (self.start_date.isocalendar().week or self.end_date.isocalendar().week) == curWeek:
+            return True
+        return False
+
+    def buildURL(name, id):
+        nameHolder = []
+        for char in name:
+            if char != " ":
+                nameHolder.append(char)
+            else:
+                nameHolder.append("-")
+        formatted_name = "".join(nameHolder)
+         #test print - print(formatted_name)
+
+
+
+class employee(object):
+    pass
+
+def insertTournament(tourn):
+    pass
+
+def main():
+    # create calendar instance to build out months and weeks with.
+    cal = Calendar()
+    months = dict.fromkeys(MONTHS)
+    testTourn = tournament("The Sentry Test", "R2024054", datetime.date(2025, 8, 23), datetime.date(2025, 8, 30))
+
+    # loop to build out calendar
+
+    for month in MONTHS:
+        months[month] = cal.monthdatescalendar(YEAR, MONTHS.index(month) + 1)
+    
+    #print(months)
+    
+    """
+    Calendar is built out for current YEAR. Now need to collect tournament data from source file or API call and parse it.
+    Once tournament data is collected and parsed, loop through all tournaments and insert them into the calendar.
+    """
+    
+    
+
+
+    
+
+if __name__ == "__main__":
+    main()
